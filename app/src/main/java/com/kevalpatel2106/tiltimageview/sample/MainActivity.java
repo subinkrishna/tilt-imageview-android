d6c41db04a5d2a5f0bf9467d830fe15a9f3a214c
Keval Patel <kevalpatel2106@gmail.com>
2017-06-07 09:45:44 +0530

    Tilt detection

--- app/src/main/java/com/kevalpatel2106/tiltimageview/sample/MainActivity.java
+++ app/src/main/java/com/kevalpatel2106/tiltimageview/sample/MainActivity.java
@@ -3,11 +3,24 @@ package com.kevalpatel2106.tiltimageview.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.kevalpatel2106.tiltimageview.TiltImageView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TiltImageView mTiltImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTiltImageView = (TiltImageView) findViewById(R.id.tilt_image_view);
+        mTiltImageView.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mTiltImageView.stop();
     }
 }
--- tiltimageview/src/main/java/com/kevalpatel2106/tiltimageview/TiltImageView.java
+++ tiltimageview/src/main/java/com/kevalpatel2106/tiltimageview/TiltImageView.java
@@ -1,10 +1,82 @@
 package com.kevalpatel2106.tiltimageview;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.kevalpatel2106.tiltimageview.sensor.Sensey;
+import com.kevalpatel2106.tiltimageview.sensor.TiltDirectionDetector;
+
 /**
  * Created by Keval Patel on 07/06/17.
  *
  * @author 'https://github.com/kevalpatel2106'
  */
 
-public class TiltImageView {
+public class TiltImageView extends ImageView{
+    private Context mContext;
+
+    /**
+     * Listener to detect tilt.
+     */
+    private TiltDirectionDetector.TiltDirectionListener mTiltDirectionListener = new TiltDirectionDetector.TiltDirectionListener() {
+        @Override
+        public void onTiltInAxisX(int direction) {
+
+        }
+
+        @Override
+        public void onTiltInAxisY(int direction) {
+
+        }
+
+        @Override
+        public void onTiltInAxisZ(int direction) {
+
+        }
+    };
+
+    public TiltImageView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public TiltImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public TiltImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @SuppressLint("NewApi")
+    public TiltImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context){
+        mContext = context;
+    }
+
+    /**
+     * Start the sensors and start tilt detection.
+     */
+    public void start(){
+        //Initialize the sensors
+        Sensey.getInstance().init(mContext);
+        Sensey.getInstance().startTiltDirectionDetection(mTiltDirectionListener);
+    }
+
+    /**
+     * Stop the sensor and stop tilt detection. Always call this in onDestroy().
+     */
+    public void stop(){
+        Sensey.getInstance().stopTiltDirectionDetection(mTiltDirectionListener);
+        Sensey.getInstance().stop();
+    }
 }
